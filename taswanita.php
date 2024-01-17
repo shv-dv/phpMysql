1b80b98066845a73515e17ff25a6b1996a1dfd45
shv-dv <156909382+shv-dv@users.noreply.github.com>
2024-01-17 22:47:23 +0700

    Add files via upload

---
+++ taswanita.php
@@ -0,0 +1,92 @@
+<?php include "header.php"; ?>				
+		<!-- Awal Panel -->
+		<div class="row">
+			<div class="panel panel-default">
+				<div class="panel-body"> 
+				<br> <h4><U> DIBAWAH INI ADALAH BARANG BARANG DARI TAS WANITA YANG TOKO KAMI JUAL</h4></U>
+				<div class="row">
+					<div class="col-md-4">
+					<h3>TAS MERAH UNDANGAN LV</h3>
+						<img src="image/001LV.png" class="img-thumbnail img-responsive">
+						<p><B> Rp.7.000.000,00 </B> <br/><a href="form.php"> <a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>TAS WANITA CREAM LIKE ICE CREAM FENDI</h3>
+						<img src="image/002fendi.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.5.000.000,00 </B> <br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>TAS WANITA SIMPLE LOUIS VUITTON </h3>
+						<img src="image/003LV.png" width="400px"  class="img-thumbnail img-responsive">
+						<p><B> Rp.3.000.000,00 </B> <br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div> 
+				</div>
+				</div>
+				<div class="row">
+					<div class="panel panel-default">
+				<div class="panel-body"> 
+				<div class="row">
+					<div class="col-md-4">
+					<h3>TAS HITAM PEKAT TORY</h3>
+						<img src="image/004tory.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.2.500.000,00 </B> <br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>TAS SATU SET GUCCI.</h3>
+						<img src="image/006g.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.10.500.000,00<br/><a href="form.php" a class="btn btn-warning btn-xs"  role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>DOMPET SIMPLE GIVENCY</h3>
+						<img src="image/008gi.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.2.000.000,00<br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="panel panel-default">
+				<div class="panel-body"> 
+				<div class="row">
+					<div class="col-md-4">
+					<h3>TAS PEGANG TANGAN SATU GUCCI</h3>
+						<img src="image/004tory.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.3.500.000,00 </B> <br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>SLING BAG GIVENCY.</h3>
+						<img src="image/009gi.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.6.000.000,00<br/><a href="form.php" a class="btn btn-warning btn-xs"  role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>TAS SLEMPANG TORY COKLAT</h3>
+						<img src="image/110tory.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.7.000.000,00<br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="panel panel-default">
+				<div class="panel-body"> 
+				<div class="row">
+					<div class="col-md-4">
+					<h3>TAS MEWAH HITAM</h3>
+						<img src="image/111pv.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.10.500.000,00 </B> <br/><a href="form.php" a class="btn btn-warning btn-xs" role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					<div class="col-md-4">
+					<h3>SATU SET FRADA.</h3>
+						<img src="image/112fr.png" width="400px" class="img-thumbnail img-responsive">
+						<p><B> Rp.20.000.000,00<br/><a href="form.php" a class="btn btn-warning btn-xs"  role="button">MASUKKAN KERANJANG</a></p>
+					</div>
+					</div>
+				</div>
+		</div><!-- Akhir Panel -->
+		</div><!--  Akhir Page -->
+		
+	<?php include "header.php"; ?>
+	
+	<script src="bootstrap/js/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<script src="bootstrap/js/dataTables.bootstrap.min.js"></script>
+	<script src="bootstrap/js/jquery.dataTables.js"></script>
+	<script src="bootstrap/js/scripts.js"></script>
+	</body>
+</html>
